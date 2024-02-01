6246c77139d3e4c5c578f3fef68c4d496004bb0e
archlinux-fan <97098618+archlinux-fan@users.noreply.github.com>
2024-02-01 21:17:34 +0100

    Create base_tools.sh

---
+++ base_tools.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+
+# icons and theme
+pacman -S arc-gtk-theme arc-icon-theme simple-and-soft-cursor papirus-icon-theme
+
+# internet
+pacman -S file-roller 
+
+# internet
+pacman -S chromium brave-bin 
+
+# mediia
+pacman -S guvcview mpv mtpaint
+
+
+baobab # disk tool?
+bleachbit # cleaning tool
+
+catfish # search tool?
+celluloid # (najbrš ne rabim - simple media player)
+
+firejail # za testiranje brez da si fillaš .config mapo
+
+galculator
+ghostscript
+
+gpick
+grub
+gsimplecal
+
+gvfs
+gvfs-mtp
+hardinfo
+libreoffice-still
+
+lightdm
+lightdm-gtk-greeter
+lightdm-gtk-greeter-settings
+
+
+mousepad
+
+neofetch
+network-manager-applet
+
+nss-mdns
+
+
+pavucontrol
+peek
+
+reflector
+ristretto
+rnote
+rofi
+scrot
+sudo
+thunar
+thunar-archive-plugin
+thunar-volman
+transmission-gtk
+ttf-ms-fonts
+vdpauinfo
+wireplumber
+xf86-video-amdgpu
+xf86-video-vesa
+xfce4-battery-plugin
+xfce4-clipman-plugin
+xfce4-panel
+xfce4-power-manager
+xfce4-pulseaudio-plugin
+xfce4-screensaver
+xfce4-screenshooter
+xfce4-session
+xfce4-settings
+xfce4-taskmanager
+xfce4-terminal
+xfce4-time-out-plugin
+xfce4-weather-plugin
+xfdesktop
+xfwm4
+xfwm4-themes
+xorg-bdftopcf
+xorg-docs
+xorg-font-util
+xorg-fonts-100dpi
+xorg-fonts-75dpi
+xorg-fonts-encodings
+xorg-iceauth
+xorg-mkfontscale
+xorg-server
+xorg-server-common
+xorg-server-devel
+xorg-server-xephyr
+xorg-server-xnest
+xorg-server-xvfb
+xorg-sessreg
+xorg-setxkbmap
+xorg-smproxy
+xorg-x11perf
+xorg-xbacklight
+xorg-xcmsdb
+xorg-xcursorgen
+xorg-xdpyinfo
+xorg-xdriinfo
+xorg-xev
+xorg-xgamma
+xorg-xhost
+xorg-xinit
+xorg-xinput
+xorg-xkbcomp
+xorg-xkbevd
+xorg-xkbutils
+xorg-xkill
+xorg-xlsatoms
+xorg-xlsclients
+xorg-xpr
+xorg-xrandr
+xorg-xrefresh
+xorg-xset
+xorg-xsetroot
+xorg-xvinfo
+xorg-xwayland
+xorg-xwd
+xorg-xwininfo
+xorg-xwud
+yay
+zram-generator
+
+
+
+# music
+# yay -S audacious-gtk3 audacious-plugins-gtk3
