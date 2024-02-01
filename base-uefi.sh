dd529c95c0af253fc77877ee3e1f625d56af3aef
archlinux-fan <97098618+archlinux-fan@users.noreply.github.com>
2024-02-01 19:57:39 +0100

    Update base-uefi.sh

--- base-uefi.sh
+++ base-uefi.sh
@@ -14,34 +14,54 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 echo root:password | root
 
+useradd -m marko
+echo marko:password | root
+usermod -G wheel,storage,audio,video,power -s /bin/bash marko
+
+# Install grub and fix boot (os-prober rabiš če imaš še windows particijo)
+pacman -S grub efibootmgr dosfstools mtools
+
+
+# Run grub install
+grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+
+# če imaš še windows particijo potem rabiš
+pacman -S os-prober
+sudo nano /etc/default/grub
+# uncomment GRUB_DISABLE_OS_PROBER_FALSE
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+
+
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
+grub-mkconfig -o /boot/grub/grub.cfg
+
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-pacman -Si grub efibootmgr # networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+# pacman -S grub efibootmgr networkmanager bluez bluez-utils # network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
 
 # pacman -S amd-ucode xf86-video-amdgpu
 # pacman -S intel-ucode 
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
-
-grub-mkconfig -o /boot/grub/grub.cfg
-
 systemctl enable NetworkManager
 systemctl enable bluetooth
-systemctl enable cups.service
+# systemctl enable cups.service
 # systemctl enable sshd
-systemctl enable avahi-daemon
+# systemctl enable avahi-daemon
 # systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
-systemctl enable reflector.timer
-systemctl enable fstrim.timer
+# systemctl enable reflector.timer
+# systemctl enable fstrim.timer
 # systemctl enable libvirtd
-systemctl enable firewalld
+# systemctl enable firewalld
 # systemctl enable acpid
 
-useradd -m marko
-echo marko:password | root
-usermod -G wheel, storage, audio, video, power -s /bin/bash marko
+
+
+
+
 
 # echo "marko ALL=(ALL) ALL" >> /etc/sudoers.d/marko - to ne dela
 printf "EDITOR=nano visudo"
