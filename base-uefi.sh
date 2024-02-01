359c4322b0612a06f8e624058c40960fb3e3ee02
archlinux-fan <97098618+archlinux-fan@users.noreply.github.com>
2024-02-01 18:19:11 +0100

    Update base-uefi.sh

--- base-uefi.sh
+++ base-uefi.sh
@@ -17,9 +17,10 @@ echo root:password | root
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-pacman -Si grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+pacman -Si grub efibootmgr # networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
 
-pacman -S amd-ucode xf86-video-amdgpu
+# pacman -S amd-ucode xf86-video-amdgpu
+# pacman -S intel-ucode 
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
@@ -40,10 +41,12 @@ systemctl enable firewalld
 
 useradd -m marko
 echo marko:password | root
-usermod -aG libvirt marko
+usermod -aG wheel, storage, audio, video, power, -s /bin/bash marko
 
-echo "marko ALL=(ALL) ALL" >> /etc/sudoers.d/marko
-
-
+# echo "marko ALL=(ALL) ALL" >> /etc/sudoers.d/marko - to ne dela
+printf "EDITOR=nano visudo"
+printf "Uncomment %wheel ... "
+printf "Add the below line which makes it so that you don't have to reinsert password in each terminal a user uses sudo in"
+printf Defaults !tty_tickets"
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
 
