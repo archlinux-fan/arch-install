8b4c87bd1c4c10fb35789cc6c1aa10360e5c0419
archlinux-fan <97098618+archlinux-fan@users.noreply.github.com>
2024-02-01 20:54:54 +0100

    Update base-uefi.sh

--- base-uefi.sh
+++ base-uefi.sh
@@ -21,42 +21,33 @@ usermod -G wheel,storage,audio,video,power -s /bin/bash marko
 # Install grub and fix boot (os-prober rabiš če imaš še windows particijo)
 pacman -S grub efibootmgr dosfstools mtools
 
-
-
-# če imaš še windows particijo in rabiš dual boot potem:
-pacman -S os-prober
-sudo nano /etc/default/grub
-# uncomment GRUB_DISABLE_OS_PROBER_FALSE
-
-# For Windows installed in UEFI mode, make sure the EFI system partition containing the Windows Boot Manager (bootmgfw.efi) is mounted. Run os-prober as root to detect and generate an entry for it
-mkdir /mnt/win11
-mount /dev/ime_win_EFI_particije /mnt/win
-
 # Run grub install
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# You can add xorg to the installation packages, I usually add it at the DE or WM install script
-# You can remove the tlp package if you are installing on a desktop or vm
-
-# pacman -S grub efibootmgr networkmanager bluez bluez-utils # network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
 
 # pacman -S amd-ucode xf86-video-amdgpu
 # pacman -S intel-ucode 
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
+
+# You can add xorg to the installation packages, I usually add it at the DE or WM install script
+# You can remove the tlp package if you are installing on a desktop or vm
+
+# pacman -S networkmanager bluez bluez-utils base-devel linux-headers reflector firewalld
+# network-manager-applet dialog wpa_supplicant avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset  flatpak sof-firmware nss-mdns acpid terminus-font
+
 systemctl enable NetworkManager
 systemctl enable bluetooth
+systemctl enable firewalld
+systemctl enable reflector.timer
+systemctl enable fstrim.timer
+
 # systemctl enable cups.service
 # systemctl enable sshd
 # systemctl enable avahi-daemon
-# systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
-# systemctl enable reflector.timer
-# systemctl enable fstrim.timer
-# systemctl enable libvirtd
-# systemctl enable firewalld
 # systemctl enable acpid
-
+# systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
 
 
 
