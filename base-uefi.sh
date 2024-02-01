fb0742358156245e370366ee456aa0368aa5b7b7
archlinux-fan <97098618+archlinux-fan@users.noreply.github.com>
2024-02-01 21:17:30 +0100

    Update base-uefi.sh

--- base-uefi.sh
+++ base-uefi.sh
@@ -19,7 +19,7 @@ echo marko:password | root
 usermod -G wheel,storage,audio,video,power -s /bin/bash marko
 
 # Install grub and fix boot (os-prober rabiš če imaš še windows particijo)
-pacman -S grub efibootmgr dosfstools mtools
+pacman -S grub efibootmgr dosfstools mtools base-devel
 
 # Run grub install
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
@@ -29,7 +29,7 @@ grub-mkconfig -o /boot/grub/grub.cfg
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-# pacman -S networkmanager bluez bluez-utils base-devel linux-headers reflector firewalld
+pacman -S bash-completion bluez bluez-utils cups firewalld linux-headers networkmanager reflector
 # network-manager-applet dialog wpa_supplicant avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset  flatpak sof-firmware nss-mdns acpid terminus-font
 
 systemctl enable NetworkManager
@@ -45,19 +45,20 @@ systemctl enable fstrim.timer
 # systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
 
 
-
+# sound
+pacman -S pipewire pipewire-pulse
 
 # echo "marko ALL=(ALL) ALL" >> /etc/sudoers.d/marko - to ne dela
 printf "EDITOR=nano visudo"
 printf "Uncomment %wheel ... "
 printf "Add the below line which makes it so that you don't have to reinsert password in each terminal a user uses sudo in"
 printf "Defaults !tty_tickets"
-
-printf "microcode + graphic card drivers install"
-printf "pacman -S amd-ucode xf86-video-amdgpu"
-printf "pacman -S intel-ucode"
-printf "pacman -S --noconfirm nvidia nvidia-utils nvidia-settings"
 printf " "
-
+printf "microcode + graphic card drivers install"
+printf "pacman -S amd-ucode xf86-video-amdgpu libva-mesa-driver libva-utils libva-vdpau-driver" # amd grafika
+printf "pacman -S intel-ucode" # intel grafika
+printf "pacman -S --noconfirm nvidia nvidia-utils nvidia-settings" # nvidia grafika
+printf "mesa-utils mesa-vdpau" # za hardware acceleration?
+printf " "
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
 
