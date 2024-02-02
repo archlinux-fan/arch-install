b05080a20c9b128ede9fcd1cb1e1ccf88fe11edf
archlinux-fan <97098618+archlinux-fan@users.noreply.github.com>
2024-02-02 21:39:38 +0100

    Update base_uefi.sh

--- base_uefi.sh
+++ base_uefi.sh
@@ -92,9 +92,7 @@ systemctl enable reflector.timer
 # trim SSD drive
 systemctl enable fstrim.timer
 
-sudo pacman -S 
-sudo pacman -S 
-sudo pacman -S 
+
 # network-manager-applet dialog wpa_supplicant avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset  flatpak sof-firmware nss-mdns acpid terminus-font
 
 # systemctl enable sshd
