982053c5569f856e8d965724b5280036438349a1
archlinux-fan <97098618+archlinux-fan@users.noreply.github.com>
2024-02-03 07:00:56 +0100

    Update and rename base_uefi.sh to 01-install-base-uefi.sh

--- 01-install-base-uefi.sh
+++ 01-install-base-uefi.sh
@@ -96,7 +96,7 @@ systemctl enable fstrim.timer
 # network-manager-applet dialog wpa_supplicant avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset  flatpak sof-firmware nss-mdns acpid terminus-font
 
 # systemctl enable sshd
-# systemctl enable acpid
+# systemctl enable acpid 
 # systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
 
 
