7deee20d3c5c069de181d94c49408afa8a05fead
archlinux-fan <97098618+archlinux-fan@users.noreply.github.com>
2024-02-03 21:34:06 +0100

    Update 06-graphic-card-testing.sh

--- 06-graphic-card-testing.sh
+++ 06-graphic-card-testing.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+printf "Accelerated video decoding "
+pacman -S libva-mesa-driver # VA-API drivers
+pacman -S mesa-vdpau # VDPAU drivers
+
+# Vulkan - ko directX, ne rabiš za youtube
+# https://wiki.archlinux.org/title/Vulkan
+# pacman -S vulkan-radeon # VDPAU drivers
+
 # Hardware acceleration info: https://wiki.archlinux.org/title/Hardware_video_acceleration
 # Verifying VA-API
 # Verify the settings for VA-API by running vainfo, which is provided by 
