8fbb3aa2154413a9249ea4241714dfc5de296a7a
archlinux-fan <97098618+archlinux-fan@users.noreply.github.com>
2024-02-03 10:44:22 +0100

    Update 02-install-base-tools.sh

--- 02-install-base-tools.sh
+++ 02-install-base-tools.sh
@@ -3,11 +3,11 @@
 # file system
 # gvfs - auto mounting disks and partitions? 
 # gvfs-mtp - auto mounting phone
-sudo pacman -S file-roller gvfs thunar thunar-archive-plugin thunar-volman
+pacman -S file-roller gvfs gvfs-mtp thunar thunar-archive-plugin thunar-volman
 
 # internet
-# sudo pacman -S chromium 
-sudo pacman -S transmission-gtk
+# pacman -S chromium 
+pacman -S transmission-gtk
 
 # media
 # guvcview - web-cam settings
@@ -15,7 +15,7 @@ sudo pacman -S transmission-gtk
 # scrot - screenshot tool
 # mtpaint
 
-sudo pacman -S guvcview mpv ristretto scrot
+pacman -S guvcview mpv ristretto scrot
 
 # Other
 # rofi - tool za search/run
@@ -25,28 +25,21 @@ sudo pacman -S guvcview mpv ristretto scrot
 # gpick - color picker tool (I never use it)
 # gsimplecal - calendar (I never use it)
 # bash-completion - use tab to complete words in bash
-sudo pacman -S rofi mousepad bash-completion
+pacman -S bash-completion mousepad rofi
 
 # System info tools
-# sudo pacman -S neofetch
-# sudo pacman -S vdpauinfo
+# pacman -S neofetch
+# pacman -S vdpauinfo
 
 # Office docs
 # libreoffice-still
 
+# Other other
 # baobab - disk tool?
 # bleachbit - cleaning tool
-
 # catfish - search tool?
 # celluloid - (najbrš ne rabim - simple media player)
-
 # firejail - za testiranje brez da si fillaš .config mapo
-
 # galculator - calculator (I never use it)
-# ghostscript - dont know
-
 
 # zram-generator
-
-
-# nss-mdns - dont know. For printer?
