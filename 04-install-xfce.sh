0a92176b340484a5dc10c51284ae98b9dd63bfb0
archlinux-fan <97098618+archlinux-fan@users.noreply.github.com>
2024-02-03 10:21:41 +0100

    Update 04-install-xfce.sh

--- 04-install-xfce.sh
+++ 04-install-xfce.sh
@@ -10,10 +10,13 @@ pacman -S network-manager-applet
 pacman -S pavucontrol # rabiš ga, da štelaš sound, ko klikneš v panelu na xfce4-pulseaudio-plugin
 
 
-# icons and theme
+# theme - morda ne rabim, ker staknem nordic z yay potem
+# pacman -S arc-gtk-theme arc-icon-theme 
 
-pacman -S arc-gtk-theme arc-icon-theme papirus-icon-theme
+# icons
+pacman -S papirus-icon-theme
 
+# install xfce
 pacman -S xfce4-battery-plugin xfce4-clipman-plugin xfce4-notifyd xfce4-panel xfce4-pulseaudio-plugin xfce4-screensaver xfce4-screenshooter xfce4-session xfce4-settings xfce4-taskmanager xfce4-terminal xfce4-time-out-plugin xfce4-weather-plugin xfdesktop xfwm4 xfwm4-themes
 
 # xfce4-power-manager - for laptop battery, za desktop tudi ampak brez veze. Za diske dat v sleep ipd
