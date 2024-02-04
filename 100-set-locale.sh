7cc816ab755b6e17f739144ab8efb9b5bccb8ff4
archlinux-fan <97098618+archlinux-fan@users.noreply.github.com>
2024-02-04 14:59:19 +0100

    Update 100-set-locale.sh

--- 100-set-locale.sh
+++ 100-set-locale.sh
@@ -7,18 +7,64 @@
 ##################################################################################################################
 
 
-# Edit /etc/locale.gen and uncomment en_US.UTF-8 UTF-8 and other needed UTF-8 locales. Or just add them at the bottom of the file (It's easier to find anyway):
+
+echo
+tput setaf 2
+echo "########################################### Choose locales ###########################################"; tput sgr0; tput setaf 3
+echo "#"
+echo "# Edit /etc/locale.gen and uncomment en_US.UTF-8 UTF-8 and other needed UTF-8 locales"
+echo "# Or just add them at the bottom of the file (It's easier to find anyway):"
+echo "######################################################################################################"
+tput sgr0; 
+echo
+
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "sl_SI.UTF-8 UTF-8" >> /etc/locale.gen
 
-# Generate the locales by running:
+
+
+echo
+tput setaf 2
+echo "######################################### Generate the locales ###########################################"; tput sgr0; tput setaf 3
+echo "#"
+echo "# Generate locales that were added or uncommented in /etc/locale.gen in the previous step"
+echo "# by running: locale-gen"
+echo "######################################################################################################"
+tput sgr0; 
+echo
+
 locale-gen
 
-# If you set the console keyboard layout, make the changes persistent in /etc/vconsole.conf and generates /etc/X11/xorg.conf.d/00-keyboard.conf
-sudo localectl set-keymap slovene
 
-# Create the /etc/locale.conf file, and set the LANG variable accordingly. Select prefered languages:
+
+echo
+tput setaf 2
+echo "################################# Set the console keyboard layout ####################################"; tput sgr0; tput setaf 3
+echo "#"
+echo "# If you set the console keyboard layout, make the changes persistent in file /etc/vconsole.conf"
+echo "# It generates file /etc/X11/xorg.conf.d/00-keyboard.conf too"
+echo "# If you don't know which one you need use the 'localectl list-keymaps' command"
+echo "######################################################################################################"
+tput sgr0
+echo
+
+localectl set-keymap slovene
+
+
+
+echo
+tput setaf 2
+echo "############################################ Set locales #############################################"; tput sgr0; tput setaf 3
+echo "#"
+echo "# Create the /etc/locale.conf file, and set the LANG variable accordingly"
+echo "# Select prefered languages: (It will add LANG=en_US.UTF-8 by default)"
+echo "######################################################################################################"
+tput sgr0
+echo
+
 sudo localectl set-locale LANG=en_US.UTF-8
+
+
 
 echo
 tput setaf 3
@@ -29,9 +75,20 @@ echo "################################################################"
 tput sgr0
 echo
 
-read response
 
-if [[ "$response" == [yY] ]]; then
+echo
+tput setaf 2
+echo "######################################### Add slovenian stuff ########################################"; tput sgr0; tput setaf 3
+echo "#"
+echo "Do you want add stuff for slovenian date, measurmenets (C), time format,...?"
+echo "Confirm with the 'y', 'Y', or just the Enter key"
+echo "######################################################################################################"
+tput sgr0
+echo
+
+read response
+if [[ "$response" == [yY] || -z "$response" ]]; then
+    # Code to execute if the response is either 'y', 'Y', or just the Enter key
     echo "LC_CTYPE=sl_SI.UTF-8
     LC_NUMERIC=sl_SI.UTF-8
     LC_TIME=sl_SI.UTF-8
@@ -48,25 +105,36 @@ if [[ "$response" == [yY] ]]; then
 fi
 
 
-# Set the font persistent in /etc/vconsole.conf
+
+echo
+tput setaf 2
+echo "############################################ Set the fonts ###########################################"; tput sgr0; tput setaf 3
+echo "#"
+echo "Set the font persistent in /etc/vconsole.conf. All available fonts are located at: ls /usr/share/kbd/consolefonts/"
+echo "If you don't know which one you to use the 'ls /usr/share/kbd/consolefonts/' command"
+echo "For Slovenians it's 'lat2-16'"
+echo "######################################################################################################"
+tput sgr0
+echo
+
 echo "FONT=lat2-16" >> /etc/vconsole.conf
 
 
 
 echo
-tput setaf 2
-echo "################################################################"
-echo "################### Done"
-echo "################################################################"
+tput setab 2
+echo "######################################### Locales are all set ########################################";
 tput sgr0
 echo
 
+
+
 echo
-tput setaf 2
-echo "################################################################"
-echo "################### rebooting now after 5 seconds"
+tput setab 1
+echo "###################################### Rebooting now in 5 seconds #####################################"; tput sgr0; tput setaf 1
+echo "#"
 echo "Press CTRL +C to stop script"
-echo "################################################################"
+echo "######################################################################################################"
 tput sgr0
 echo
 
