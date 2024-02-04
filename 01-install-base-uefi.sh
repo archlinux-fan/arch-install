6b97b5dc5bf08ce905baa3d065a7f66046576e35
archlinux-fan <97098618+archlinux-fan@users.noreply.github.com>
2024-02-04 06:45:27 +0100

    Update 01-install-base-uefi.sh

--- 01-install-base-uefi.sh
+++ 01-install-base-uefi.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+
+
+
+# Website   : https://www.arcolinuxiso.com
+# Website   : https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+#tput setaf 0 = black
+#tput setaf 1 = red
+#tput setaf 2 = green
+#tput setaf 3 = yellow
+#tput setaf 4 = dark blue
+#tput setaf 5 = purple
+#tput setaf 6 = cyan
+#tput setaf 7 = gray
+#tput setaf 8 = light blue
+##################################################################################################################
+
+installed_dir=$(dirname $(readlink -f $(basename `pwd`)))
+
+##################################################################################################################
+
+echo
+tput setaf 2
+echo "################################################################"
+echo "################### Making sure KDFONTOP at boot is gone"
+echo "################################################################"
+tput sgr0
+echo
+
+
+
+
 # Set the time zone:
 ln -sf /usr/share/zoneinfo/Europe/Ljubljana /etc/localtime
 
@@ -123,6 +159,11 @@ printf " "
 printf " "
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
 
+
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
 
 
 : <<'END_COMMENT'
