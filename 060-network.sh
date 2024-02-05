4f268ee6c0cae44616151c53f6ed037be96612a5
archlinux-fan <97098618+archlinux-fan@users.noreply.github.com>
2024-02-05 10:13:48 +0100

    Create 060-network.sh

---
+++ 060-network.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+##################################################################################################################
+# I got inspiration from:
+# Author    : Erik Dubois 
+# Github    : https://github.com/erikdubois
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+# network (there is no other wifi tool otherwise. Use iwctl )
+sudo pacman -S networkmanager
+
+# applet to run in a panel inside DE
+pacman -S network-manager-applet
+
+
+systemctl enable NetworkManager
