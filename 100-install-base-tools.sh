9166d94fd5cd4df4e3c1a3215387b4aed3d8fe26
archlinux-fan <97098618+archlinux-fan@users.noreply.github.com>
2024-02-05 12:33:12 +0100

    Update 100-install-base-tools.sh

--- 100-install-base-tools.sh
+++ 100-install-base-tools.sh
@@ -9,8 +9,8 @@ sudo pacman -S thunar-archive-plugin    # Thunar plugin for handling archives
 sudo pacman -S thunar-volman            # Thunar plugin for managing removable devices
 
 # Internet
-#sudo pacman -S chromium                 # Open-source web browser based on the Blink rendering engine        
-#yay -S brave                            # I use brave, which is installed later from AUR.Open-source web browser based on the Blink rendering engine        
+# sudo pacman -S chromium               # Open-source web browser based on the Blink rendering engine        
+# yay -S brave                          # I use brave, which is installed later from AUR.Open-source web browser based on the Blink rendering engine        
 sudo pacman -S transmission-gtk         # BitTorrent client with a user-friendly GTK interface
 
 # Media
@@ -18,14 +18,14 @@ sudo pacman -S guvcview                 # Tool for viewing and adjusting webcam 
 sudo pacman -S mpv                      # Versatile media player with support for various formats
 sudo pacman -S ristretto                # Simple and fast image viewer
 sudo pacman -S scrot                    # Command-line screenshot tool for capturing and saving images
-#sudo pacman -S mtpaint                 # Simple and lightweight graphical paint program
-#sudo pacman -S peek                    # Tool for recording short screencasts as animated GIFs
+# sudo pacman -S mtpaint                # Simple and lightweight graphical paint program
+# sudo pacman -S peek                   # Tool for recording short screencasts as animated GIFs
 
 # Tools
-#sudo pacman -S rnote                    # Feature-rich and visually appealing text editor
-#sudo pacman -S gpick                    # Advanced color picker tool for selecting and manipulating colors
-#sudo pacman -S gsimplecal               # Simple calendar application for basic date management
-#sudo pacman -S firejail                 # Sandbox tool for safely running applications without affecting system configuration
+# sudo pacman -S rnote                  # Feature-rich and visually appealing text editor
+# sudo pacman -S gpick                  # Advanced color picker tool for selecting and manipulating colors
+# sudo pacman -S gsimplecal             # Simple calendar application for basic date management
+# sudo pacman -S firejail               # Sandbox tool for safely running applications without affecting system configuration
 
 # Other
 sudo pacman -S bash-completion          # Provides tab completion for Bash commands and options
@@ -33,27 +33,27 @@ sudo pacman -S mousepad                 # Xfce's default text editor
 sudo pacman -S rofi                     # Fast and flexible tool for application launching and searching
 
 # Multimedia
-#sudo pacman -S galculator               # Scientific calculator with advanced mathematical functions
+# sudo pacman -S galculator             # Scientific calculator with advanced mathematical functions
 
 # Office docs
-#sudo pacman -S libreoffice-still        # Full-featured office suite for creating and editing documents
+# sudo pacman -S libreoffice-still      # Full-featured office suite for creating and editing documents
 
 # System info tools
 sudo pacman -S neofetch                 # Displays system information in a stylish format
 sudo pacman -S vdpauinfo                # Provides information about VDPAU capabilities (video acceleration)
 
 # System Management
-#sudo pacman -S zram-generator # Tool for creating and managing zram devices (compressed RAM used as swap space)
+# sudo pacman -S zram-generator         # Tool for creating and managing zram devices (compressed RAM used as swap space)
 
 # Other other
-#sudo pacman -S baobab                   # Disk usage analyzer for visualizing disk space usage
-#sudo pacman -S bleachbit                # System cleaning tool for removing temporary files and caches
-#sudo pacman -S catfish                  # Desktop search tool for finding files quickly
-#sudo pacman -S firejail                 # Sandbox tool for safely running applications without affecting system configuration
+# sudo pacman -S baobab                 # Disk usage analyzer for visualizing disk space usage
+# sudo pacman -S bleachbit              # System cleaning tool for removing temporary files and caches
+# sudo pacman -S catfish                # Desktop search tool for finding files quickly
+# sudo pacman -S firejail               # Sandbox tool for safely running applications without affecting system configuration
 
 # Application Manager
-#sudo pacman -S flatpak                  # Application manager for installing and managing self-contained sandboxes 
-                                         # with software regardless of system distribution.
+# sudo pacman -S flatpak                # Application manager for installing and managing self-contained sandboxes 
+                                        # with software regardless of system distribution.
 
 
 
