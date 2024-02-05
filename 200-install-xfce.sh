f2ce0dca7ae159b5ad8e4eac5f08ead7833221fa
archlinux-fan <97098618+archlinux-fan@users.noreply.github.com>
2024-02-05 12:31:00 +0100

    Update 200-install-xfce.sh

--- 200-install-xfce.sh
+++ 200-install-xfce.sh
@@ -1,49 +1,52 @@
 #!/bin/bash
 
-# login manager
-pacman -S lightdm 
-pacman -S lightdm-gtk-greeter
-pacman -S lightdm-gtk-greeter-settings
+# Login manager
+sudo pacman -S lightdm                          # Lightweight and fast login manager
+sudo pacman -S lightdm-gtk-greeter              # GTK 3 greeter for lightdm
+sudo pacman -S lightdm-gtk-greeter-settings     # Settings tool for lightdm-gtk-greeter
 
-# enable the lightdm service
-systemctl enable lightdm
-
-# package is a GTK 3 front-end which works under Xorg environments with a systray
-pacman -S network-manager-applet
-
-# package is needed to set sound levels in XFCE panel like xfce4-pulseaudio-plugin
-pacman -S pavucontrol
-
-# death icon to kill apps with a mouse
-pacman -S xorg-xkill
-
-# theme - morda ne rabim, ker staknem nordic z yay potem
-# pacman -S arc-gtk-theme arc-icon-theme 
-
-# icons
-pacman -S papirus-icon-theme
-
-# install xfce and stuff
-pacman -S xfce4-battery-plugin
-pacman -S xfce4-clipman-plugin 
-pacman -S xfce4-notifyd 
-pacman -S xfce4-panel
-pacman -S xfce4-pulseaudio-plugin 
-pacman -S xfce4-screensaver
-pacman -S xfce4-screenshooter 
-pacman -S xfce4-session 
-pacman -S xfce4-settings 
-pacman -S xfce4-taskmanager 
-pacman -S xfce4-terminal 
-pacman -S xfce4-time-out-plugin 
-pacman -S xfce4-weather-plugin 
-pacman -S xfdesktop 
-pacman -S xfwm4 
-pacman -S xfwm4-themes
+# Enable the lightdm service
+sudo systemctl enable lightdm
 
 
-# xfce4-power-manager - for laptop battery, za desktop tudi ampak brez veze. Za diske dat v sleep ipd
-# pacman -S xfce4-power-manager
+# Network manager applet
+sudo pacman -S network-manager-applet           # GTK 3 front-end for NetworkManager
 
 
-printf "reboot now to try lightdm"
+# PulseAudio volume control
+sudo pacman -S pavucontrol                      # Advanced sound level control
+
+
+# Application termination tool
+sudo pacman -S xorg-xkill                       # Allows killing applications with a mouse click
+
+
+# Themes (currently not used. nordic-bluish-accent-standard-buttons-theme theme installed later from AUR is preferred)
+# sudo pacman -S arc-gtk-theme                  # GTK theme set
+# sudo pacman -S arc-gtk-theme arc-icon-theme   # GTK icon set(nordic installed later from AUR is preferred)
+
+# Icons
+sudo pacman -S papirus-icon-theme               # Popular icon theme
+
+# Install XFCE components
+sudo pacman -S xfce4-battery-plugin             # Battery status indicator for XFCE panel
+sudo pacman -S xfce4-clipman-plugin             # Clipboard manager for XFCE panel
+sudo pacman -S xfce4-notifyd                    # Notification daemon for XFCE
+sudo pacman -S xfce4-panel                      # Main panel for XFCE
+sudo pacman -S xfce4-pulseaudio-plugin          # PulseAudio integration for XFCE panel
+sudo pacman -S xfce4-screensaver                # Screen saver for XFCE
+sudo pacman -S xfce4-screenshooter              # Screenshot tool for XFCE
+sudo pacman -S xfce4-session                    # Session management for XFCE
+sudo pacman -S xfce4-settings                   # Settings application for XFCE
+sudo pacman -S xfce4-taskmanager                # Task manager for XFCE
+sudo pacman -S xfce4-terminal                   # Terminal emulator for XFCE
+sudo pacman -S xfce4-time-out-plugin            # Automatic logout plugin for XFCE
+sudo pacman -S xfce4-weather-plugin             # Weather plugin for XFCE panel
+sudo pacman -S xfdesktop                        # Desktop environment components for XFCE
+sudo pacman -S xfwm4                            # Window manager for XFCE
+sudo pacman -S xfwm4-themes                     # Themes for the XFWM4 window manager
+
+# XFCE power manager (skip for desktop)
+# sudo pacman -S xfce4-power-manager            # Power management for laptops (not needed for desktop)
+
+
