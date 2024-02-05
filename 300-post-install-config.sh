eb52a2896e491fb5d6b31eb8ab729f19c6192864
archlinux-fan <97098618+archlinux-fan@users.noreply.github.com>
2024-02-05 12:36:01 +0100

    Update 300-post-install-config.sh

--- 300-post-install-config.sh
+++ 300-post-install-config.sh
@@ -6,17 +6,15 @@ pacman -S firewalld
 # enable the firewalld service
 systemctl enable firewalld
 
+
 # mirorrs automatic sorting 
 pacman -S reflector
 
 # enable the reflector timer to run weekly
 systemctl enable reflector.timer
 
+
 # trim SSD drive
 # enable the fstrim timer to trim SSD drive weekly
 systemctl enable fstrim.timer
-
-# systemctl enable sshd
-# systemctl enable acpid 
-# systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
 
