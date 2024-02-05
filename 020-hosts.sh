7b911800bcde7c221a0454ed6b4320ac37418d50
archlinux-fan <97098618+archlinux-fan@users.noreply.github.com>
2024-02-05 09:43:33 +0100

    Create 020-hosts.sh

---
+++ 020-hosts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# I got inspiration from:
+# Author    : Erik Dubois 
+# Github    : https://github.com/erikdubois
+##################################################################################################################
+
+# Hostname settings
+echo "Chose the hostname for your computer: (one word, small letters)"
+read hostname
+echo $hostname >> /etc/hostname
+
+# Hosts settings
+echo "127.0.0.1  localhost" >> /etc/hosts
+echo "::1        localhost" >> /etc/hosts
+echo "127.0.1.1  $hostname.localdomain $hostname" >> /etc/hosts
