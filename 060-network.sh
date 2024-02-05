10aa3d4d620ae3c183dc9c7a50c7345bc3076f18
archlinux-fan <97098618+archlinux-fan@users.noreply.github.com>
2024-02-05 11:01:03 +0100

    Update 060-network.sh

--- 060-network.sh
+++ 060-network.sh
@@ -10,7 +10,8 @@
 #
 ##################################################################################################################
 
-# network (there is no other wifi tool otherwise. Use iwctl )
+# NetworkManager is a graphical and command-line utility for managing network connections in Linux
+# I think you don't need dhcpcd since it has it's own. Not sure though
 sudo pacman -S networkmanager
 
 # enable the NetworkManager service
@@ -20,4 +21,21 @@ systemctl enable NetworkManager
 # applet to run in a panel inside DE
 # pacman -S network-manager-applet # will be installed inside XFCE script
 
+
+
+# To connect to wifi use the following commands:
+
+# List your network cards:
+# nmcli
+
+
+# List all visible Wi-Fi networks:
+# nmcli dev wifi list
+
+
+# connect
+# nmcli dev wifi connect <network_name> password <password>
+
+# Replace <network_name> with the actual name of your Wi-Fi network. Wrap the Wi-Fi network name in " " if you use space in the name. Example: nmcli dev wifi connect "Wu Tang LAN"
+# Replace <password> with the actual password of your Wi-Fi network.
 
