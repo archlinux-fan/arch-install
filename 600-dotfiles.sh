4af3ba74b48f5974cacee6449c1d8ced183ba2c7
archlinux-fan <97098618+archlinux-fan@users.noreply.github.com>
2024-02-06 08:42:03 +0100

    Update 600-dotfiles.sh

--- 600-dotfiles.sh
+++ 600-dotfiles.sh
@@ -11,6 +11,7 @@
 
 1. Install Git:
 sudo pacman -S git  
+
 2. Create a GitHub repository:
 
 Go to GitHub and create a new repository specifically for your dotfiles.
@@ -23,12 +24,18 @@ Create a folder for your dotfiles if it doesn't exist (e.g., ~/.dotfiles).
 Move your existing dotfiles into this folder.
 Navigate to the folder in your terminal and initialize Git:
 
-cd ~/.dotfiles
+cd ~/.git/.dotfiles
+
 git init --bare
 
+# Rename the "master" branch to "main" or "primary". Why? The term "master" can have connotations of power and dominance, which can be exclusionary and insensitive.
+git branch -m main
+
+# connect your local repository to a GitHub repository at https://github.com/user/repo.git, 
 # do this before the next command with the token
 git remote add origin https://github.com/archlinux-fan/dotfiles.git
 
+# set the token if you use a private github
 # git remote set-url origin https://<personal access token>@github.com/<username>/<repository>.git
 git remote set-url origin https://<personal access token>@github.com/archlinux-fan/dotfiles.git
 
@@ -57,11 +64,23 @@ dots add .bashrc
 # To add a directory to the repository:
 dots add ~/.config/audacious/
 
+# use a script to specify all the stuff at once add_config_folders.sh:
+#!/bin/bash
+dots add ~/.config/audacious 
+dots add ~/.bashrc
+
+run with:
+sh add_config_folders.sh
+
+# add your personal stuff, otherwise commit doesn't work. Če daš --global flag, potem ne bo shranilo info v repotov config file ampak na home v .gitconfig, tega pa nočeš, ker potem je za vse repote
+dots config user.name "My_name"
+dots config user.email "my.email@email.com"
+
 # To commit changes:
 dots commit -m "Updated bash settings"
 
 # To push changes to a remote repository:
-dots push origin master
+dots push origin main
 
 Key points:
 
@@ -69,3 +88,8 @@ The alias doesn't store the command within the Git repository itself, but it pro
 The alias is specific to your shell environment. If you use different shells, you'll need to set up the alias in each one.
 Remember to replace dots with your preferred alias name.
 
+# to remove the last changes
+dots reset 
+
+This command directly removes the specified file from the staging area. It won't delete the file from your filesystem unless you use the -f flag (caution: use with care as it's permanent).
+git rm <filename>
